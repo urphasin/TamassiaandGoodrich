77a279b4f16be5d1da555b71bbcb4ab4c079bea6
Otito Amuga(TTEN-Computer-Room) <urphasin@gmail.com>
2025-07-29 12:55:42 -0600

    r_8 done.

--- src/Chapter1/Exercises/Reinforcement/R_8.java
+++ src/Chapter1/Exercises/Reinforcement/R_8.java
@@ -9,11 +9,22 @@ import java.util.*;
 
 public class R_8 {
     public static int countVowels(String s) {
-        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        Set<Character> vowelSet = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        int count = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if (vowelSet.contains(s.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String s = sc.nextLine();
+
+        System.out.println(countVowels(s));
 
         sc.close();
     }
