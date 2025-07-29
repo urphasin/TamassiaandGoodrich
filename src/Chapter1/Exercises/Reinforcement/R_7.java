77aaf84a35782061762696447e32e1f9df2627be
Otito Amuga(TTEN-Computer-Room) <urphasin@gmail.com>
2025-07-29 11:39:05 -0600

    r_7 done

---
+++ src/Chapter1/Exercises/Reinforcement/R_7.java
@@ -0,0 +1,29 @@
+/*
+Write a short Java method that takes an integer n and returns the sum of the
+squares of all positive integers less than or equal to n.
+*/
+
+package Chapter1.Exercises.Reinforcement;
+
+import java.util.Scanner;
+
+public class R_7 {
+    public static int sumSquaresToN(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i * i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(sumSquaresToN(n));
+
+        sc.close();
+
+    }
+}
