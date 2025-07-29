23faa9520112e76e11bebf2b489c5614e7da9052
Otito Amuga(TTEN-Computer-Room) <urphasin@gmail.com>
2025-07-29 11:42:03 -0600

    working on r_8

---
+++ src/Chapter1/Exercises/Reinforcement/R_8.java
@@ -0,0 +1,20 @@
+package Chapter1.Exercises.Reinforcement;
+
+/*
+Write a short Java method that counts the number of vowels in a given character
+string.
+ */
+
+import java.util.*;
+
+public class R_8 {
+    public static int countVowels(String s) {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+    }
+}
